bf7d436558145b7271f52b2641fceaaae7381420
liujinmeng <1321839748@qq.com>
2017-03-19 12:59:26 +0800

    localStorage and signout

--- src/components/Header.js
+++ src/components/Header.js
@@ -110,22 +110,38 @@ import React from 'react';
 import axios from 'axios';
 
 import AppBar from 'material-ui/AppBar';
-import IconButton from 'material-ui/IconButton';
-import ActionHome from 'material-ui/svg-icons/action/home';
-import FlatButton from 'material-ui/FlatButton';
-import RaisedButton from 'material-ui/RaisedButton';
 import Dialog from 'material-ui/Dialog';
 import TextField from 'material-ui/TextField';
+
+import IconButton from 'material-ui/IconButton';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import AccountCircle from 'material-ui/svg-icons/action/account-circle';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import IconMenu from 'material-ui/IconMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+import Snackbar from 'material-ui/Snackbar';
 
 class Header extends React.Component{
   constructor(){
     super();
     this.state={
-      open: false,
-      action:'signin',
-      username:'',
-      password:'',
-      welcome:'登录/注册'
+      open: false,//登录注册表单的开关
+      action:'signin',//登录注册表单的选项
+      username:'',//表单 username
+      password:'',//表单 password
+      isLogin:false,//是否登录成功
+      user:'',//登录后后台返回的 user
+      userId:'',//登录后后台返回的 userId
+      openMenu:false,//登录后弹出式菜单的开关
+      snackBar:false,//登出时弹出的提示框
+    }
+  }
+  componentWillMount(){
+    if(localStorage.user&&localStorage.userId){
+      this.setState({isLogin:true,user:localStorage.user,userId:localStorage.userId})
     }
   }
   handleClose(){
@@ -146,10 +162,9 @@ class Header extends React.Component{
     axios.post(`http://api.duopingshidai.com/user/${this.state.action}`,data)
       .then(res => {
         console.log(res);
-        this.setState({open:false})
-        if(this.state.action=='signin'&&res.data.msg=='登陆成功'){
-          this.setState({welcome:`你好,${res.data.user}!欢迎登录!`})
-        }
+        this.setState({open:false,isLogin:true,user:res.data.user,userId:res.data.userId})
+        localStorage.setItem('user',res.data.user)
+        localStorage.setItem('userId',res.data.userId)
       })
       .catch(err => {
         if(err.response){
@@ -157,6 +172,23 @@ class Header extends React.Component{
         }else{
           console.log('Error',err);
         }
+      })
+  }
+  handleOnRequestChange(value){
+    // console.log(value);
+    this.setState({
+      openMenu: value,
+    })
+  }
+  handleItem(e,child){
+    if(child.props.value==='3') this.logout();
+  }
+  logout(){
+    axios.get('http://api.duopingshidai.com/user/logout')
+      .then( res => {
+        this.setState({isLogin:false,user:'',userId:'',snackBar:true})
+        localStorage.user = '';
+        localStorage.userId = '';
       })
   }
   render(){
@@ -179,11 +211,24 @@ class Header extends React.Component{
           <AppBar
             title="Tiger_Liu"
             iconElementLeft={<IconButton><ActionHome /></IconButton>}
-            onRightIconButtonTouchTap={this.handleOpen.bind(this)}
-            iconElementRight={<FlatButton label={this.state.welcome} />}
+            iconElementRight={
+              this.state.isLogin ?
+                <IconMenu
+                  iconButtonElement={<IconButton tooltip={`你好,${this.state.user}!`}><AccountCircle /></IconButton>}
+                  open={this.state.openMenu}
+                  onRequestChange={this.handleOnRequestChange.bind(this)}
+                  anchorOrigin={{horizontal: 'right', vertical: 'bottom'}}
+                  targetOrigin={{horizontal: 'right', vertical: 'top'}}
+                  onItemTouchTap={this.handleItem.bind(this)}
+                >
+                  <MenuItem value="1" primaryText={`你好,${this.state.user}!`} />
+                  <MenuItem value="2" primaryText="个人中心" />
+                  <MenuItem value="3" primaryText="退出"/>
+                </IconMenu> :
+                <FlatButton label='登录/注册' onTouchTap={this.handleOpen.bind(this)}/>
+            }
           />
         </div>
-        {/* <div className='loginout'>{this.state.welcome!='登录/注册'&&this.state.welcome!='' ? <RaisedButton label='退出'/> :  <div></div>}</div> */}
         <Dialog
           title="用户表单"
           actions={actions}
@@ -197,6 +242,14 @@ class Header extends React.Component{
           <TextField hintText="Username" floatingLabelText="username" onChange={this.handleUsername.bind(this)}/><br/>
           <TextField hintText="Password" floatingLabelText="Password" type="password" onChange={this.handlePassword.bind(this)}/><br/><br />
         </Dialog>
+
+        <Snackbar
+          open={this.state.snackBar}
+          message="登出成功"
+          autoHideDuration={2000}
+          onRequestClose={() => this.setState({snackBar:false})}
+          bodyStyle={{textAlign:'center'}}
+        />
       </div>
     )
   }
